03893791dd7dd8ab3676d0b86fe29ce9cadb31bc
Jonghyun <hamjong-won@hamjong-won-ui-MacBookPro.local>
2021-02-09 01:42:27 +0900

    ECMP, outgoing interface added

--- Dijkstra_minheap/main.cpp
+++ Dijkstra_minheap/main.cpp
@@ -7,12 +7,13 @@
 std::ifstream fin("graph.txt");
 std::vector<Node> *head;
 int *cost;
-std::vector<int> *next_hop; //next hop info
+std::vector<std::unordered_map<int,std::string>> *nexthop_info; // next hop info
 std::vector<int> check; //to check if visited
 std::vector<int> *parents;
+
 Heap_funcs func;
 // source node(starting point 0 -> A, 1 -> B ...)
-int src_node = 0;
+int src_node = 1;
 
 int name2int(char c)
 {
@@ -26,10 +27,12 @@ char int2name(int i)
 void adjGraph(int* V, int* E){
   char vertex[3];
   int w;
+  std::string oif; //outgoing interface
+  std::string iif; //interface
   fin >> *V >> *E;
   head = new std::vector<Node>[*V];
   cost = new int[*V];
-  next_hop = new std::vector<int>[*V];
+  nexthop_info = new std::vector<std::unordered_map<int,std::string>>[*V];
   parents = new std::vector<int>[*V];
 
   //init : set check to 0
@@ -38,11 +41,11 @@ void adjGraph(int* V, int* E){
   }
 
   for (int i = 0; i < *E; i++) {
-    fin >> vertex >> w;
+    fin >> vertex >> w >> oif >> iif;
     int from = name2int(vertex[0]);
     int to = name2int(vertex[1]);
-    Node node(to, w);
-    Node node_mirror(from, w);
+    Node node(to, w, oif);
+    Node node_mirror(from, w, iif);
 
     head[from].push_back(node);
     head[to].push_back(node_mirror);
@@ -60,51 +63,56 @@ void printGraph(int V){
   }
 };
 
-//only starting point is cost 0, else : infinite(here : 1000)
+  //only starting point is cost 0, else : infinite(here : 1000)
 void cost_init(int s, int V) {
   for(int i = 0 ; i < V; i++) {
     if(i == s) cost[i] = 0;
     else cost[i] = 1000;
-    // parent[i].push_back(-1);
+      // parent[i].push_back(-1);
   }
 }
 
-//recursive
+  //recursive
 void dijkstra(std::vector<Node> &heap, int s, int V){
   std:: vector<Node>:: iterator it;
-  check[s] = 1;
+  check[s] = 1; //check visited node
   for(it = head[s].begin(); it != head[s].end(); it++){
+  //if it is yet to visited
     if(check[(*it).get_val()]) continue;
-    //if it is yet to visited
-    int d = cost[s] + (*it).get_weight();
     
+    int d = cost[s] + (*it).get_weight();
     if( d < cost[(*it).get_val()]) {
         cost[(*it).get_val()] = d;
-        Node update((*it).get_val(), d);
-        //inherit the next_hop from parent
-        next_hop[(*it).get_val()] = next_hop[s];
+        Node update((*it).get_val(), d, (*it).get_oif());
+  //inherit the next_hop from parent
+        nexthop_info[(*it).get_val()] = nexthop_info[s];
+
         func.upheap(update, heap);
         if(parents[(*it).get_val()].size() >=1) parents[(*it).get_val()].back() = s;
         else parents[(*it).get_val()].push_back(s);
     }
-    //ECMP : if d is same , then append 
+  //ECMP : if d is same , then append 
     else if(d == cost[(*it).get_val()]) {
         cost[(*it).get_val()] = d;
-        Node update((*it).get_val(), d);
-        //push_back because it is ECMP case
-        next_hop[(*it).get_val()].push_back(next_hop[s][0]);
+        Node update((*it).get_val(), d, (*it).get_oif());
+  //push_back because it is ECMP case
+        nexthop_info[(*it).get_val()].push_back((nexthop_info[s][0]));
+
         func.upheap(update, heap);
         parents[(*it).get_val()].push_back(s);
     }
   }
-  //check heap is empty (size 1) 
+  //check if heap is empty (size 1) 
   if(heap.size() <= 1) return;
   Node min = func.extract_min(heap);
   
   //if parent of min is src_node : this min would be one of next hops
   if(parents[min.get_val()].back() == src_node) {
-    next_hop[min.get_val()].push_back(min.get_val());
+    std::unordered_map<int, std::string> x;
+    x[min.get_val()] = min.get_oif();
+    nexthop_info[min.get_val()].push_back(x);
   } 
+  //recursive
   dijkstra(heap, min.get_val(), V);
 }
 
@@ -145,9 +153,9 @@ int main() {
   int V,E;
   
   std::vector <Node> heap;
-  std::vector <int>:: iterator it;
+  std::vector <std::unordered_map<int, std::string>>:: iterator it;
   //heap init (dummy heap[0])
-  Node empty(30,10000);
+  Node empty(30,10000, "empty");
   heap.insert(heap.begin(), empty);
 
   if(!fin){
@@ -165,8 +173,9 @@ int main() {
 
   for(int i = 0 ; i < V; i++){
     std::cout <<"\n if you want to go : " << int2name(i) <<", next hop : ";
-    for(it = next_hop[i].begin(); it != next_hop[i].end(); it++){
-      std::cout << int2name(*it) << ",";
+    for(it = nexthop_info[i].begin(); it != nexthop_info[i].end(); it++){
+       for(auto& i : *it)
+          std::cout << int2name(i.first) << ", " << i.second << "   ";
     }
     std::cout << " cost :" << cost[i] <<'\n';
   }
@@ -174,5 +183,5 @@ int main() {
   delete [] parents;
   delete [] head;
   delete [] cost;
-  delete [] next_hop;
+  delete [] nexthop_info;
 }
