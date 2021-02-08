137f38f9af70e24064172e9356d7452561d7b53e
Jonghyun <hamjong-won@hamjong-won-ui-MacBookPro.local>
2021-02-08 19:07:44 +0900

    ECMP added

--- Dijkstra_minheap/main.cpp
+++ Dijkstra_minheap/main.cpp
@@ -7,10 +7,12 @@
 std::ifstream fin("graph.txt");
 std::vector<Node> *head;
 int *cost;
-int *next_hop; //next hop info
+std::vector<int> *next_hop; //next hop info
 std::vector<int> check; //to check if visited
-std::vector<int> *parent;
+std::vector<int> *parents;
 Heap_funcs func;
+// source node(starting point 0 -> A, 1 -> B ...)
+int src_node = 0;
 
 int name2int(char c)
 {
@@ -27,7 +29,7 @@ void adjGraph(int* V, int* E){
   fin >> *V >> *E;
   head = new std::vector<Node>[*V];
   cost = new int[*V];
-  next_hop = new int[*V];
+  next_hop = new std::vector<int>[*V];
   parents = new std::vector<int>[*V];
 
   //init : set check to 0
@@ -63,7 +65,7 @@ void cost_init(int s, int V) {
   for(int i = 0 ; i < V; i++) {
     if(i == s) cost[i] = 0;
     else cost[i] = 1000;
-    // parent.push_back(-1);
+    // parent[i].push_back(-1);
   }
 }
 
@@ -75,64 +77,75 @@ void dijkstra(std::vector<Node> &heap, int s, int V){
     if(check[(*it).get_val()]) continue;
     //if it is yet to visited
     int d = cost[s] + (*it).get_weight();
+    
     if( d < cost[(*it).get_val()]) {
         cost[(*it).get_val()] = d;
         Node update((*it).get_val(), d);
+        //inherit the next_hop from parent
+        next_hop[(*it).get_val()] = next_hop[s];
         func.upheap(update, heap);
         if(parents[(*it).get_val()].size() >=1) parents[(*it).get_val()].back() = s;
         else parents[(*it).get_val()].push_back(s);
-	}
-    //ECMP : if d is same , then append
+    }
+    //ECMP : if d is same , then append 
     else if(d == cost[(*it).get_val()]) {
-      std::cout << " in==cost :" << d; 
         cost[(*it).get_val()] = d;
         Node update((*it).get_val(), d);
+        //push_back because it is ECMP case
+        next_hop[(*it).get_val()].push_back(next_hop[s][0]);
         func.upheap(update, heap);
         parents[(*it).get_val()].push_back(s);
     }
-}
+  }
   //check heap is empty (size 1) 
   if(heap.size() <= 1) return;
   Node min = func.extract_min(heap);
+  
+  //if parent of min is src_node : this min would be one of next hops
+  if(parents[min.get_val()].back() == src_node) {
+    next_hop[min.get_val()].push_back(min.get_val());
+  } 
   dijkstra(heap, min.get_val(), V);
 }
 
-void set_nexthop(int s, int V){
-  int j;
-  for(int i = 0 ; i < V; i++) {
-    if(parent[i].size() == 1){
-      if((i == s) || (parent[i][0] == s)) {
-        next_hop[i].push_back(i);
-        continue;
-      }
-      j = i;
-      while(parent[j].back() != s) {
-        j = parent[j].back();
-      }
-      next_hop[i].push_back(j);
-    } 
-    //for ECMP
-     else if(parent[i].size() >= 2) {
-      std::vector <int>:: iterator it;
-      for(it = parent[i].begin(); it != parent[i].end(); it++){
-        j = *it;
-        while(parent[j].back() != s) {
-          j = parent[j].back();
-        }
-        next_hop[i].push_back(j);
-      }
-    } 
-    //source node
-    else {
-      next_hop[i].push_back(i);
-    }
-  }
-}
+// 2) derive next hop from parent info
+// void set_nexthop(int s, int V){
+//   int j;
+//   for(int i = 0 ; i < V; i++) {
+//     if(parents[i].size() == 1){
+//       if((i == s) || (parents[i][0] == s)) {
+//         next_hop[i].push_back(i);
+//         continue;
+//       }
+//       j = i;
+//       while(parents[j].back() != s) {
+//         j = parents[j].back();
+//       }
+//       next_hop[i].push_back(j);
+//     } 
+//     //for ECMP
+//      else if(parents[i].size() >= 2) {
+//       std::vector <int>:: iterator it;
+//       for(it = parents[i].begin(); it != parents[i].end(); it++){
+//         j = *it;
+//         while(parents[j].back() != s) {
+//           j = parents[j].back();
+//         }
+//         next_hop[i].push_back(j);
+//       }
+//     } 
+//     //source node
+//     else {
+//       next_hop[i].push_back(i);
+//     }
+//   }
+// }
 
 int main() {
   int V,E;
-  int src_node = 0;
+  
   std::vector <Node> heap;
+  std::vector <int>:: iterator it;
   //heap init (dummy heap[0])
   Node empty(30,10000);
   heap.insert(heap.begin(), empty);
@@ -146,9 +159,9 @@ int main() {
   printGraph(V);
   cost_init(src_node, V);
   dijkstra(heap, src_node, V);
-  set_nexthop(src_node, V);
+  // set_nexthop(src_node, V);
 
-  std::cout << "from Source Node: "<< src_node << '\n';
+  std::cout << "\n from Source node " <<  int2name(src_node) << '\n';
 
   for(int i = 0 ; i < V; i++){
     std::cout <<"\n if you want to go : " << int2name(i) <<", next hop : ";
@@ -157,8 +170,8 @@ int main() {
     }
     std::cout << " cost :" << cost[i] <<'\n';
   }
-
-  delete [] parent;
+   
+  delete [] parents;
   delete [] head;
   delete [] cost;
   delete [] next_hop;
